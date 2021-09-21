2e1bfaf05f2bf6a29b01e337ab91b45303a83e3f
Inácio Silva <inacio.silva.1999@gmail.com>
2021-09-21 10:27:00 -0300

    Fazendo classe car

--- src/services/CreateCarService.ts
+++ src/services/CreateCarService.ts
@@ -4,15 +4,28 @@ import { TagsRepositories } from "../repositories/TagsRepositories";
 
 class CreateCarService {
     async execute(name, brand: string) {
+        const allBrands = ["Fiat", "Chevrolet", "Ford", "Volkswagen", "Volvo", "Ferrari", "Audi"]
 
+        if (!name) {
+            throw new Error("No name!");
+        }
+
+        if (!brand) {
+            throw new Error("No brand!");
+        }
+
+        const existsBrand = allBrands.find(element => element === brand) == null ? false : true;
+
+        if (existsBrand) {
+
+        }
 
     }
 }
 
 export { CreateCarService }
 
-
-/* 
+/*
     Verificar se nome e marca do carro está com letra maiúscula,
     Verificar se a marca existe no JSON que vou criar.
-*/ 
+*/
